2cbd3b048ac23c4b5c5fe6222c2dc6e42121b93d
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-12-18 08:15:38 +0530

    button ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { PlusIcon } from "./icons/PlusIcon";
 
 function App() {
-   
+  function Add() {}
 
   return (
     <>
-      
+      <Button startIcon={<div className="flex"> <PlusIcon size="lg" />  </div> } variant="primary" size="md" text="delete" onClick={Add}></Button>
+      <Button variant="secondary" size="md" text="share" onClick={Add}></Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
