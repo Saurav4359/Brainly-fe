696d22e7be62128edbbd824dfe0fb56348782cdf
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-12-18 10:34:18 +0530

    button -mistake

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Button startIcon={<div className="flex"> <PlusIcon size="lg" />  </div> } variant="primary" size="md" text="delete" onClick={Add}></Button>
-      <Button variant="secondary" size="md" text="share" onClick={Add}></Button>
+      <Button startIcon={<div className="flex"> <PlusIcon size="lg" />  </div> } variant="secondary" size="md" text="Share" onClick={Add}></Button>
     </>
   );
 }
